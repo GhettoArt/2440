9e5bcdd6eb45d87c7fb5b9a1dc6edc5bdad84be3
Thomas Belin <thomasbelin4@gmail.com>
2014-12-22 19:25:31 +0100

    add the image location in the artists json file

--- artists.json.js
+++ artists.json.js
@@ -8,6 +8,7 @@ parseArtists([ {% for artist in site.artists %}
             "date": "{{ artist.date }}",
             "hour": "{{ artist.hour }}",
             "stage": "{{ artist.stage }}",
+            "image": "/images/artists/{{ artist.name | slugify }}.jpg",
             "description": "{{ artist.content | strip_newlines | replace: '"': '\"' | replace: '</': '<\/' | replace: '/>': ' />' }}"
         }{% unless forloop.last %},{% endunless %}
     {% endfor %}
