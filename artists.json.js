39cdeeccdb258ab0d0c2e00ae3a32e24e7bef4c6
Gildas Quéméner <gildas.quemener@gmail.com>
2014-12-22 19:01:15 +0100

    Fixed description html escaping

--- artists.json.js
+++ artists.json.js
@@ -8,7 +8,7 @@ parseArtists([ {% for artist in site.artists %}
             "date": "{{ artist.date }}",
             "hour": "{{ artist.hour }}",
             "stage": "{{ artist.stage }}",
-            "description": "{{ artist.content | strip_newlines }}"
+            "description": "{{ artist.content | strip_newlines | replace: '"': '\"' | replace: '</': '<\/' | replace: '/>': ' />' }}"
         }{% unless forloop.last %},{% endunless %}
     {% endfor %}
 ]);
